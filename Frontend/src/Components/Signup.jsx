ee54651211c69ea593073b0e8ba8675914180319
Aryan-Patel30 <paryan3009@gmail.com>
2025-07-25 21:22:51 +0530

    change in the signup page

--- Frontend/src/Components/Signup.jsx
+++ Frontend/src/Components/Signup.jsx
@@ -1,162 +1,178 @@
 import React from "react";
+import "../CustomCss/Signup.css";
 
 function Signup() {
   return (
-    <>
-      <div className="flex justify-center items-center h-screen">
-        <form action="" className="flex flex-col gap-4 min-w-96 p-6 rounded-md shadow-md border border-white">
-          <h1 className="text-2xl text-center">Patel Chat<span className="text-green-500 font-semibold">App</span></h1>
-          <h2>Signup</h2>
-
-          {/*username */}
-          <div>
-            <label className="input validator">
-              <svg
-                className="h-[1.1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
+    <div className="signup-container flex justify-center items-center min-h-screen">
+      <div className="signup-box-border">
+        <form className="signup-box flex flex-col gap-4 min-w-96 p-6">
+          <h1 className="signup-title text-2xl font-bold text-center text-white">
+            Patel Chat<span className="text-green-500">App</span>
+          </h1>
+          <h2 className="signup-title text-xl text-center text-white/80 mb-2">
+            Sign Up
+          </h2>
+          <div className="signup-content">
+            {/*username */}
+            <div>
+              <label className="input validator">
+                <svg
+                  className="h-[1.1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
                 >
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-                  <circle cx="12" cy="7" r="4"></circle>
-                </g>
-              </svg>
-              <input
-                type="text"
-                required
-                placeholder="Username"
-                pattern="[A-Za-z][A-Za-z0-9\-]*"
-                minlength="3"
-                maxlength="30"
-                title="Only letters, numbers or dash"
-              />
-            </label>
-            <p className="validator-hint hidden">
-              Must be 3 to 30 characters
-              <br />
-              containing only letters, numbers or dash
-            </p>
-          </div>
-          {/*email */}
-          <div>
-            <label className="input validator">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
-              </svg>
-              <input type="email" placeholder="mail@gmail.com" required />
-            </label>
-            <div className="validator-hint hidden">
-              Enter valid email address
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </g>
+                </svg>
+                <input
+                  type="text"
+                  required
+                  placeholder="Username"
+                  pattern="[A-Za-z][A-Za-z0-9\-]*"
+                  minlength="3"
+                  maxlength="30"
+                  title="Only letters, numbers or dash"
+                />
+              </label>
+              <p className="validator-hint hidden">
+                Must be 3 to 30 characters
+                <br />
+                containing only letters, numbers or dash
+              </p>
             </div>
-          </div>
-
-          {/*password */}
-          <div>
-            <label className="input validator">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
+            {/*email */}
+            <div>
+              <label className="input validator">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
                 >
-                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
-                  <circle
-                    cx="16.5"
-                    cy="7.5"
-                    r=".5"
-                    fill="currentColor"
-                  ></circle>
-                </g>
-              </svg>
-              <input
-                type="password"
-                required
-                placeholder="Password"
-                minlength="8"
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
-              />
-            </label>
-            <p className="validator-hint hidden">
-              Must be more than 8 characters, including
-              <br />
-              At least one number <br />
-              At least one lowercase letter <br />
-              At least one uppercase letter
-            </p>
-          </div>
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                  </g>
+                </svg>
+                <input type="email" placeholder="mail@gmail.com" required />
+              </label>
+              <div className="validator-hint hidden">
+                Enter valid email address
+              </div>
+            </div>
 
-          {/*confirm password */}
-          <div>
-            <label className="input validator">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
+            {/*password */}
+            <div>
+              <label className="input validator">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
                 >
-                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
-                  <circle
-                    cx="16.5"
-                    cy="7.5"
-                    r=".5"
-                    fill="currentColor"
-                  ></circle>
-                </g>
-              </svg>
-              <input
-                type="password"
-                required
-                placeholder="Confirm Password"
-                minlength="8"
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                title="Confirm Password must match the password"
-              />
-            </label>
-            <p className="validator-hint hidden">
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                    <circle
+                      cx="16.5"
+                      cy="7.5"
+                      r=".5"
+                      fill="currentColor"
+                    ></circle>
+                  </g>
+                </svg>
+                <input
+                  type="password"
+                  required
+                  placeholder="Password"
+                  minlength="8"
+                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                  title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                />
+              </label>
+              <p className="validator-hint hidden">
+                Must be more than 8 characters, including
+                <br />
+                At least one number <br />
+                At least one lowercase letter <br />
+                At least one uppercase letter
+              </p>
+            </div>
+
+            {/*confirm password */}
+            <div>
+              <label className="input validator">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                    <circle
+                      cx="16.5"
+                      cy="7.5"
+                      r=".5"
+                      fill="currentColor"
+                    ></circle>
+                  </g>
+                </svg>
+                <input
+                  type="password"
+                  required
+                  placeholder="Confirm Password"
+                  minlength="8"
+                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                  title="Confirm Password must match the password"
+                />
+              </label>
+              <p className="validator-hint hidden">
                 Confirm Password must match the password
-            </p>
-          </div>
+              </p>
+            </div>
 
-          {/*Text and submit button */}
-          <div>
-            <p>Have an account? Login</p>
-            <input type="submit" value="Signup" />
+            {/*Text and submit button */}
+            <div className="flex flex-col gap-4">
+              <button
+                type="submit"
+                className="submit-btn py-2 px-4 rounded-lg text-white font-semibold hover:shadow-lg transition-all"
+              >
+                Sign Up
+              </button>
+              <p className="text-center text-white/70">
+                Already have an account?{" "}
+                <a href="/login" className="text-blue-400 hover:underline">
+                  Login
+                </a>
+              </p>
+            </div>
           </div>
         </form>
       </div>
-    </>
+    </div>
   );
 }
 
