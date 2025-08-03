e4c3b2aa6715b0473a96617aaf25dc9dba4f9991
Aryan-Patel30 <paryan3009@gmail.com>
2025-08-03 16:39:08 +0530

    Minor Change

--- Frontend/src/home/Rightpart/Chatuser.jsx
+++ Frontend/src/home/Rightpart/Chatuser.jsx
@@ -12,7 +12,7 @@ function Chatuser() {
   return (
     <div className="flex space-x-3 items-center h-[9vh] justify-center p-1 bg-gray-800 hover:bg-gray-700 duration-300 ">
       <div className={`avatar avatar-${getOnlineUsersStatus(selectedConversation._id)}`}> 
-        <div className="w-13 rounded-full ring-primary ring-offset-base-100 ring-2 ring-offset-2 overflow-hidden">
+        <div className="w-12 rounded-full ring-primary ring-offset-base-100 ring-2 ring-offset-2 overflow-hidden">
           <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
         </div>
       </div>
