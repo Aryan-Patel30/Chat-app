7210dffbc3ed91545bed50ff353d05784adfbdb6
Aryan-Patel30 <paryan3009@gmail.com>
2025-08-14 12:41:16 +0530

    Origin allowed

--- Backend/index.js
+++ Backend/index.js
@@ -1,27 +1,29 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import userRoutes from './routes/userRoute.js';
-import messageRoutes from './routes/messageRoute.js';
-import { app, server } from './SocketIO/server.js';
-
+import userRoutes from "./routes/userRoute.js";
+import messageRoutes from "./routes/messageRoute.js";
+import { app, server } from "./SocketIO/server.js";
 
 dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MONGODB_URI;
 
 try {
-  mongoose.connect(URI)
+  mongoose.connect(URI);
 } catch (error) {
   console.error(error);
 }
